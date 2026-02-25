6dfd4069c3ed7c5f1c57757f5ac411c86fade5bf
AtiladDias <128435253+AtiladDias@users.noreply.github.com>
2026-02-25 13:42:07 -0300

    Create custom Document component for Next.js

---
+++ src/pages/_document.tsx
@@ -0,0 +1,16 @@
+// src/pages/_document.tsx
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="base:app_id" content="699f1f2174175638d96bf342" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
